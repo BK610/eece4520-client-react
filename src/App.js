f574485da20d8c221eafe6d5c88918218ae1a669
Bailey Kane <bailey.orion.kane@gmail.com>
2019-03-24 15:59:52 -0400

    barebones ui

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react';
 import './App.css';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import Analysis from './components/Analysis';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <h1>
+                    Bot or Not
+                </h1>
+
+                <Router>
+                    <div>
+                        <input placeholder="Twitter URL or handle"/>
+                        <Link to="/analysis">
+                            <button>
+                                Analyze!
+                            </button>
+                        </Link>
+                        <Route
+                            path="/analysis"
+                            exact
+                            component={Analysis}/>
+                    </div>
+                </Router>
+            </div>
+        );
+    }
 }
 
 export default App;
