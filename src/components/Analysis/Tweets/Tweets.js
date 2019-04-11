2bf4bc1e3e1235c7ab51ed74104663b32ad5ad04
Bailey Kane <bailey.orion.kane@gmail.com>
2019-04-11 12:44:43 -0400

    Update display of tweets

--- src/components/Analysis/Tweets/Tweets.js
+++ src/components/Analysis/Tweets/Tweets.js
@@ -5,15 +5,27 @@ import Col from "react-bootstrap/Col";
 export default function Tweets(props) {
     return (
         <div>
+            <Row>
+                <Col xs={2}>
+                    <b>
+                        ID
+                    </b>
+                </Col>
+                <Col xs={10}>
+                    <b>
+                        Tweet
+                    </b>
+                </Col>
+            </Row>
             {props.tweets ?
                 props.tweets
                     .map(tweet =>
                         <Row key={tweet.id}>
-                            <Col>
-                                id: {tweet.id}
+                            <Col xs={2}>
+                                {tweet.id}
                             </Col>
-                            <Col>
-                                text: {tweet.text}
+                            <Col xs={10}>
+                                {tweet.text}
                             </Col>
                         </Row>
                     )
