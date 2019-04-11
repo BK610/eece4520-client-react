32eb475e67589f0e90512f59b52aafe31dd6c702
Bailey Kane <bailey.orion.kane@gmail.com>
2019-04-11 15:47:51 -0400

    Remove ID column

--- src/components/Analysis/Tweets/Tweets.js
+++ src/components/Analysis/Tweets/Tweets.js
@@ -5,26 +5,11 @@ import Col from "react-bootstrap/Col";
 export default function Tweets(props) {
     return (
         <div>
-            <Row>
-                <Col xs={2}>
-                    <b>
-                        ID
-                    </b>
-                </Col>
-                <Col xs={10}>
-                    <b>
-                        Tweet
-                    </b>
-                </Col>
-            </Row>
             {props.tweets ?
                 props.tweets
                     .map(tweet =>
                         <Row key={tweet.id}>
-                            <Col xs={2}>
-                                {tweet.id}
-                            </Col>
-                            <Col xs={10}>
+                            <Col>
                                 {tweet.text}
                             </Col>
                         </Row>
