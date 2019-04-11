238156287cdba8ea0458f3233b588493d9769abf
Bailey Kane <bailey.orion.kane@gmail.com>
2019-04-11 15:46:40 -0400

    Adjust image display

--- src/components/Analysis/About/About.js
+++ src/components/Analysis/About/About.js
@@ -4,36 +4,40 @@ import Row from "react-bootstrap/Row";
 
 export default function About(props) {
     return (
-            <Row>
-                <Col>
-                    Profile image:
-                    <img src={props.user.profileImageUrl}/>
-                </Col>
-                <Col>
-                    <div>
-                        name: {props.user.name}
-                    </div>
-                    <div>
-                        screenname: {props.user.screenName}
-                    </div>
-                    <div>
-                        location: {props.user.location}
-                    </div>
-                </Col>
-                <Col>
-                    <div>
-                        # tweets: {props.user.statusesCount}
-                    </div>
-                    <div>
-                        # followers: {props.user.followersCount}
-                    </div>
-                    <div>
-                        # following: {props.user.friendsCount}
-                    </div>
-                    <div>
-                        # favorites: {props.user.favouritesCount}
-                    </div>
-                </Col>
-            </Row>
+        <Row>
+            <Col>
+                Profile image:
+                <div className=".profile-picture">
+                    <img
+                        src={props.user.profileImageUrl}
+                        onError={props.handleImageNotFound}/>
+                </div>
+            </Col>
+            <Col>
+                <div>
+                    name: {props.user.name}
+                </div>
+                <div>
+                    screenname: {props.user.screenName}
+                </div>
+                <div>
+                    location: {props.user.location}
+                </div>
+            </Col>
+            <Col>
+                <div>
+                    # tweets: {props.user.statusesCount}
+                </div>
+                <div>
+                    # followers: {props.user.followersCount}
+                </div>
+                <div>
+                    # following: {props.user.friendsCount}
+                </div>
+                <div>
+                    # favorites: {props.user.favouritesCount}
+                </div>
+            </Col>
+        </Row>
     )
 }
